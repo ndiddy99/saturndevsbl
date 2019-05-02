4e4ad4d0a9f4b1ed8568a70d5deb8034234446f9
Nathan Misner <nathan@thotpatrol.pw>
2019-05-02 12:55:10 -0400

    got background converter working

--- tools/bgconv/bgconv.dpr
+++ tools/bgconv/bgconv.dpr
@@ -15,37 +15,38 @@ begin
   WriteLn('-oc or -ob: output to c file or binary (default: c file)');
 end;
 
-//procedure outputC(var cFile: TextFile; varName: String; tiles: array of Uint16; palette: array of Uint32);
-//var
-//  i, j: Integer;
-//begin
-//  WriteLn(cFile, 'const Uint16 ', varName, '[] = {');
-//  for i := 0 to Length(img) - 1 do
-//  begin
-//    for j := 0 to Length(img[0]) - 1 do
-//    begin
-//      if (j < Length(img[0]) - 1) or (i < Length(img) - 1) then
-//        Write(cFile, img[i][j], ',')
-//      else
-//        Write(cFile, img[i][j]);
-//    end;
-//    Write(cFile, #13#10); //new line
-//  end;
-//  WriteLn(cFile, '};');
-//end;
+procedure outputC(var cFile: TextFile; varName: String; tiles: array of Uint8; palette: array of Uint32);
+var
+  i: Integer;
+begin
+  WriteLn(cFile, 'const Uint32 ', varName, '_pal[] = {');
+  for i := 0 to Length(palette) - 1 do
+  begin
+    Write(cFile, palette[i], ',');
+  end;
+  WriteLn(cFile, '};');
+  WriteLn(cFile, 'const Uint8 ', varName, '_chr[] = {');
+  for i := 0 to Length(tiles) do
+  begin
+    Write(cFile, tiles[i], ',');
+    if (i + 1) mod 8 = 0 then
+      Write(cFile, #13#10);
+  end;
+  WriteLn(cFile, '};');
+end;
 
 procedure handleImage(input: String; outType: Integer);
 var
   img: TBitmap;
   imgPal: TMaxLogPalette;
-  line: pByte;
-  saturnTiles: array of Uint16;
+  line: pByteArray;
+  saturnTiles: array of Uint8;
   //Saturn palette format:
   //0123456789ABCDEF0123456789ABCDEF
   //RRRRRRRRGGGGGGGGBBBBBBBB00000000
-  saturnPal: array of Uint32;
-  i, j, count: Integer;
-  r, g, b: Uint8;
+  saturnPal: array[0..255] of Uint32;
+  i, j, k, l, count: Integer;
+  r, g, b: Uint32;
   outFile: TextFile;
   varName: String;
 begin
@@ -55,20 +56,72 @@ begin
   img.LoadFromFile(input);
   SetLength(saturnTiles, img.Height * img.Width);
   GetPaletteEntries(img.Palette, 0, 256, imgPal.palPalEntry);
-  WriteLn('num entries: ', imgPal.palNumEntries);
-  for i := 0 to 256 do
+  for i := 0 to 255 do
   begin
-    WriteLn('r: ', imgPal.palPalEntry[i].peRed, 'g: ', imgPal.palPalEntry[i].peGreen,
-      'b: ', imgPal.palPalEntry[i].peBlue);
+    r := imgPal.palPalEntry[i].peRed;
+    g := imgPal.palPalEntry[i].peGreen;
+    b := imgPal.palPalEntry[i].peBlue;
+//    WriteLn('r: ', imgPal.palPalEntry[i].peRed, 'g: ', imgPal.palPalEntry[i].peGreen,
+//      'b: ', imgPal.palPalEntry[i].peBlue);
+    saturnPal[i] := r or (g shl 8) or (b shl 16);
+  end;
+  i := 0;
+  j := 0;
+  count := 0;
+  while i < img.Height - 1 do
+  begin
+    while j < img.Height - 1 do
+    begin
+      for k := i to (i + 8) - 1 do //top left
+      begin
+        line := pByteArray(img.ScanLine[k]);
+        for l := j to (j + 8) - 1 do
+        begin
+          saturnTiles[count] := line[l];
+          Inc(count);
+        end;
+      end;
+
+      for k := i to (i + 8) - 1 do //top right
+      begin
+        line := pByteArray(img.ScanLine[k]);
+        for l := j + 8 to (j + 16) - 1 do
+        begin
+          saturnTiles[count] := line[l];
+          Inc(count);
+        end;
+      end;
+
+      for k := i + 8 to (i + 16) - 1 do //bottom left
+      begin
+        line := pByteArray(img.ScanLine[k]);
+        for l := j to (j + 8) - 1 do
+        begin
+          saturnTiles[count] := line[l];
+          Inc(count);
+        end;
+      end;
+
+      for k := i + 8 to (i + 16) - 1 do //bottom right
+      begin
+        line := pByteArray(img.ScanLine[k]);
+        for l := j + 8 to (j + 16) - 1 do
+        begin
+          saturnTiles[count] := line[l];
+          Inc(count);
+        end;
+      end;
+      j := j + 16; //position: top pixel + 16
+    end;
+    i := i + 16; //inc y pos
   end;
 
 
-
-  //if outType = 0 then
-  //begin
-  //  varName := copy(input[i], 0, pos('.', input[i]) - 1);
-  //  outputC(outFile, varName, saturnImg);
-  //end;
+  if outType = 0 then
+  begin
+    varName := copy(input, 0, pos('.', input) - 1);
+    outputC(outFile, varName, saturnTiles, saturnPal);
+  end;
 //  else
 //    outputBin(saturnImg);
   img.Destroy;
