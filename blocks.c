e7b27ef360d0fcf6d97c14e98de0c3e9085fb57f
Nathan Misner <nathan@thotpatrol.pw>
2020-01-06 23:41:12 -0500

    bit more gfx

--- blocks.c
+++ blocks.c
@@ -5,7 +5,7 @@
 Uint8 tile2_slope[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 16};
 Uint8 tile3_slope[] = {0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7};
 Uint8 tile4_slope[] = {8, 8, 9, 9, 10, 10, 11, 11, 12, 12, 13, 13, 14, 14, 15, 15};
-Uint8 *block_slopes[] = {NULL, NULL, tile2_slope, tile3_slope, tile4_slope, NULL};
+Uint8 *block_slopes[] = {NULL, NULL, tile2_slope, tile3_slope, tile4_slope, NULL, NULL, NULL, NULL, tile2_slope, tile3_slope, tile4_slope};
 
 Uint8 block_get(int tile, int offset) {
     Uint8 *slope_ptr = block_slopes[(tile & 0x1ff) >> 1];
